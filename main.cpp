380133e8ad7282cc6617bf1e03ab1d102b76f11d
Ateveu <37016340+Ateveu@users.noreply.github.com>
2021-05-07 12:26:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -112,27 +112,14 @@ void heapsort(int data[], int n){
 void merge(int arr[], int l, int m, int r){
     int n1 = m - l + 1;
     int n2 = r - m;
-
-    // Create temp arrays
     int L[n1], R[n2];
-
-    // Copy data to temp arrays L[] and R[]
     for (int i = 0; i < n1; i++)
         L[i] = arr[l + i];
     for (int j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
-
-    // Merge the temp arrays back into arr[l..r]
-
-    // Initial index of first subarray
     int i = 0;
-
-    // Initial index of second subarray
     int j = 0;
-
-    // Initial index of merged subarray
     int k = l;
-
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
             arr[k] = L[i];
@@ -144,17 +131,11 @@ void merge(int arr[], int l, int m, int r){
         }
         k++;
     }
-
-    // Copy the remaining elements of
-    // L[], if there are any
     while (i < n1) {
         arr[k] = L[i];
         i++;
         k++;
     }
-
-    // Copy the remaining elements of
-    // R[], if there are any
     while (j < n2) {
         arr[k] = R[j];
         j++;
@@ -162,12 +143,9 @@ void merge(int arr[], int l, int m, int r){
     }
 }
 
-// l is for left index and r is
-// right index of the sub-array
-// of arr to be sorted */
 void mergeSort(int arr[],int l,int r){
     if(l>=r){
-        return;//returns recursively
+        return;
     }
     int m =l+ (r-l)/2;
     mergeSort(arr,l,m);
@@ -295,7 +273,6 @@ int main()
     //shellsort(v, n);
     //heapsort(v, n);
     //mergeSort(v, 0, n-1);
-    itmergeSort(v, 0, n-1);
     //quicksort(v, n);
     //radixsort(v, n);
     //countingsort(v, n);
